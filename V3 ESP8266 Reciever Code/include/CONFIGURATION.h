5fbaa0e5a4343688e6f77f95046caaf830ef81e2
Parth Patel <parth.pmech@gmail.com>
2021-10-13 23:59:32 +0530

    Clean the code

---
+++ V3 ESP8266 Reciever Code/include/CONFIGURATION.h
@@ -0,0 +1,21 @@
+// This file is meant to store the data to wonnect to wifi and the aws configuration data
+#ifndef CONFIGURATION_H // In order to avoid double mentions and compiler errors
+#define CONFIGURATION_H
+
+#define WIFI_ON // In order to determine is model has Wifi turned on
+
+//! AWS IOT config details. These are the credentials to connect with AWS and with the Wifi:
+#ifdef WIFI_ON
+char WIFI_SSID[] = "project105";
+char WIFI_PASSWORD[] = "project105";
+#endif
+char AWS_ENDPOINT[] = "ayru2i5e2th7e-ats.iot.us-east-2.amazonaws.com";
+char AWS_KEY[] = "AKIAQTTS4PUTBSHUMA47";
+char AWS_SECRET[] = "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";
+char AWS_REGION[] = "ap-south-1";
+const char *AWS_TOPIC = "$aws/things/ESP8266-Console/shadow/name/IoT_Core/update"; // Used to update value to AWS server
+int PORT = 443;
+const int MQTT_PORT = 8883;
+const char MQTT_SUB_TOPIC[] = "aws/command22/123456789";
+const char MQTT_PUB_TOPIC[] = "$aws/rules/123456789/aws/tel/123456789";
+#endif
--- V3 ESP8266 Reciever Code/src/main.cpp
+++ V3 ESP8266 Reciever Code/src/main.cpp
@@ -21,16 +21,30 @@ TODO 9. Upload Data to AWS
 */
 
 #include <Arduino.h>
-#include "connect_to_wifi_and_ntp.h"
+#include <ESP8266WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include "CONFIGURATION.h"
 //#include "AWS_Setup.h"
+
+// Define NTP Client to get time
+const long utcOffsetInSeconds = 19800; //+ 5:30 hr = 19800 sec
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+
+// ! Function Definition
+void connect_to_wifi(void);
+void connect_to_ntp(void);
+void update_time_via_ntp(void);
+
 void setup()
 {
-        //! New Code
-        Serial.begin(115200);
-        delay(3000);
-        Serial.println("\n\n");
-        Serial.println("Attempting to connect to Wifi");
-        connect_to_wifi_and_ntp();
+//! New Code
+#ifdef WIFI_ON
+        connect_to_wifi();
+        connect_to_ntp();
+#endif
         //! Old Code
         // NTPConnect();
         // net.setCACert(cacert);
@@ -52,7 +66,9 @@ void setup()
 
 void loop()
 {
-        update_ntp_time();
+#ifdef WIFI_ON
+        update_time_via_ntp();
+#endif
         // now = time(nullptr);
         // if (!client.connected())
         // {
@@ -74,4 +90,54 @@ void loop()
 
         //     OLED_print("MSG Sent", String((millis() - lastMillis) / 1000) + "sec", "AGO");
         // }
+}
+void connect_to_wifi(void)
+{ /**
+ * @brief This funciton searches for wifi and tries connecting it.
+ * If it connects to wifi then it displays "Wifi Connection success!", else it displays "Wifi Connection Error!"
+ * 
+ */
+        Serial.begin(115200);
+        delay(3000);
+        Serial.println("\n\nAttempting to connect to Wifi");
+        WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+        int connection_attempt = 0;
+        while ((WiFi.status() != WL_CONNECTED) & (connection_attempt < 8))
+        {
+                delay(50);
+                Serial.print(".");
+                connection_attempt++;
+        }
+        switch (WiFi.status())
+        {
+        case 3: //WL_CONNECTED = 3: Connection Successful
+                Serial.println("Wifi connect success of SSID" + String(WIFI_SSID));
+                break;
+        case 1: //WL_NO_SSID_AVAIL = 1: Wifi not in range
+                Serial.println("Cannot Find SSID, Check if router in Range!");
+                break;
+        case 4: //WL_CONNECT_FAILED = 4: Connection failed
+                Serial.println("Wifi Connection Failed!");
+                break;
+        default: // Cannot find the error due to which wifi cannot be connected
+                Serial.println("Wifi Connection Error! Cannot Diagnose Issue");
+                break;
+        }
+}
+
+void connect_to_ntp()
+{
+        timeClient.begin();
+}
+
+void update_time_via_ntp()
+{
+        for (size_t i = 0; i < 3; i++)
+        {
+                timeClient.update();
+        }
+        Serial.print(daysOfTheWeek[timeClient.getDay()]);
+        Serial.print(", ");
+        Serial.println(timeClient.getFormattedTime());
+        delay(1000);
 }
